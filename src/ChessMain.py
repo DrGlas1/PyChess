350375910fd387be3f4888541c3d6b6b10b1c18f
Elliot Glas <elliot.glas@hotmail.se>
2022-12-05 15:23:05 +0100

    Now draws pieces and is able to move pieces

--- src/ChessMain.py
+++ src/ChessMain.py
@@ -12,6 +12,8 @@ DIMENSION = 8                           # Chessboard size
 SQUARE_SIZE = WIDTH // DIMENSION        # Square size
 MAX_FPS = 15                            # Highest number of frames per second for animation
 IMAGES = {}                             # Images for displaying the chess pieces
+LIGHT_SQUARE_COLOR = "White"            # The color of the light squares
+DARK_SQUARE_COLOR = "Grey"              # The color of the dark squares
 
 
 def load_images():
@@ -29,13 +31,33 @@ def main():
     gs = ChessEngine.GameState()
     load_images()
     running = True
+    sq_selected = ()                                   # Tuple with the square that the player last selected
+    player_clicks = []                                 # Array of the last two selected squares
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()           # (x, y) location of the mouse
+                c = location[0] // SQUARE_SIZE
+                r = location[1] // SQUARE_SIZE
+                if sq_selected == (r, c):              # Clicking on the same piece deselects the clicks
+                    sq_selected = ()
+                    player_clicks = []
+                else:
+                    sq_selected = (r, c)
+                    player_clicks.append(sq_selected)
+                    print(sq_selected)
+                    print(player_clicks)
+                if len(player_clicks) == 2:
+                    move = ChessEngine.Move(player_clicks[0], player_clicks[1], gs.board)
+                    print(move.get_chess_notation())
+                    gs.make_move(move)
+                    sq_selected = ()
+                    player_clicks = []
         clock.tick(MAX_FPS)
         p.display.flip()
-        draw_game_state(screen,gs)
+        draw_game_state(screen, gs)
 
 
 def draw_game_state(screen, gs):
@@ -45,7 +67,8 @@ def draw_game_state(screen, gs):
 
 
 def draw_board(screen):
-    colors = [p.Color("White"), p.Color("Grey")]
+    """Draws the squares of the board"""
+    colors = [p.Color(LIGHT_SQUARE_COLOR), p.Color(DARK_SQUARE_COLOR)]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             # Light squares have even parity and dark squares have odd parity on a chess board
@@ -54,7 +77,12 @@ def draw_board(screen):
 
 
 def draw_pieces(screen, board):
-    pass
+    """Draws the pieces onto the current GameState.board"""
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 
 if __name__ == "__main__":
